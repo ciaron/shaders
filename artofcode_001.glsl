62ad84532377adeba1515c631081c37cd3b8e381
Ciaron Linstead <ciaron.linstead@gmail.com>
2019-04-16 10:20:19 +0200

    from art of code video

---
+++ artofcode_001.glsl
@@ -0,0 +1,26 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+void main() {
+  // .5 centres coords (otherwise 0,0 is bottom left)
+  vec2 coord = (gl_FragCoord.xy - .5*u_resolution) / u_resolution.y;
+  //vec2 coord = (gl_FragCoord.xy - 0.09*u_resolution) / u_resolution.y;
+  vec3 color = vec3(0.);
+  // black =(0,0,0), white=(1,1,1), yellow=(1,1,0) cyan=0,1,1
+  // magenta = 1,0,1
+  //gl_FragColor = vec4(0., 0., 0., 1.);
+
+  // make grid. fract reduces large number (x5) to fractional part
+  vec2 gv = fract(coord * 5.) - .5;
+  float d = length(gv);
+  float m = smoothstep(.4, .3, d);
+
+  color.rg = gv;
+  color +=m;
+  gl_FragColor = vec4(color, 1.);
+
+}
